ce7f174e49a29a36efa5da65859371be749dca1d
KevinArdiprana46 <220711827@students.uajy.ac.id>
2025-05-28 13:46:00 +0700

    Update Barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -314,4 +314,20 @@ class BarangController extends Controller
             'rating_penitip' => $penitip->rating_penitip
         ]);
     }
+
+    public function getDetailBarangById($id)
+    {
+        $barang = Barang::with(['penitip', 'penitipan', 'foto_barang'])
+            ->find($id);
+
+        if (!$barang) {
+            return response()->json([
+                'message' => 'Barang tidak ditemukan.'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $barang
+        ]);
+    }
 }
