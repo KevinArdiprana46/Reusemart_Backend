2108bedcd46d37d0dca5528e3f57e5236aac8114
KevinArdiprana46 <220711827@students.uajy.ac.id>
2025-06-28 00:41:26 +0700

    Fix Deploy 6

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    protected $namespace = 'App\\Http\\Controllers';
     public function boot()
     {
         parent::boot();
@@ -16,5 +18,26 @@ class RouteServiceProvider extends ServiceProvider
             $email = $notifiable->getEmailForPasswordReset();
             return "http://localhost:5173/reset-password?token=$token&email=$email";
         });
+    }
+
+    public function map()
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
 }
