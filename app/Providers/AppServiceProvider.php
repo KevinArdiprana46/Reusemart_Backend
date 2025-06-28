17946236ad8e01e8b9ba8e313b0cf7dc0d5ab3b4
KevinArdiprana46 <220711827@students.uajy.ac.id>
2025-06-29 03:35:35 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+            \App\Http\Middleware\SafeTrimStrings::class
+        );
     }
 
     /**
